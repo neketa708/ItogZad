3ecbe084f32155cf5ab7cb256af8c2b83d0e72d8
NEKETA <molnei1996@gmail.com>
2023-03-26 13:05:07 +0300

    added filter

--- Program.cs
+++ Program.cs
@@ -2,11 +2,32 @@
 int m = Convert.ToInt32(Console.ReadLine());
 string [] stringArray = new string [m];
 
-void array(string [] stringArray)
+void Array(string [] stringArray)
 {
   for (int i = 0;i<stringArray.Length;i++)
   {
      Console.WriteLine($"Введите {i+1} элемент массива");
      stringArray[i] = Console.ReadLine();
   }
+}
+
+string [] Symbol(string [] stringArray)
+{
+  int n = 0;
+  for (int i = 0;i<stringArray.Length;i++)
+  {
+    if(stringArray[i].Length <=3)
+    n++;
+  }
+  string [] rez = new string [n];
+  int j = 0;
+  for (int i = 0;i<stringArray.Length;i++)
+  {
+    if(stringArray[i].Length <=3)
+    {
+        rez[j] = stringArray[i];
+        j++;
+    }
+  }
+  return rez;
 }
